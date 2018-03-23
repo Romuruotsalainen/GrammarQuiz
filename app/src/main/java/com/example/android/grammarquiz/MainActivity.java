034cf7e75a14ae6de8bd32d9375ddcca55539dd7
Markus Söderman <markus.soderman@gmail.com>
2018-03-23 20:49:41 +0200

    Finished quizz app for the Udacity Nanodegree

--- app/src/main/java/com/example/android/grammarquiz/MainActivity.java
+++ app/src/main/java/com/example/android/grammarquiz/MainActivity.java
@@ -23,8 +23,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void openQuestion() {
         EditText text = findViewById(R.id.open_question);
-        String text_as_string = text.getText().toString();
+        EditText textTwo = findViewById(R.id.open_question_two);
+
+        String text_as_string = text.getText().toString().trim();
+        String text_as_string_two = textTwo.getText().toString().trim();
+
         if (text_as_string.equalsIgnoreCase("diglossia")) {
+            points += 3;
+        }
+        if (text_as_string_two.equalsIgnoreCase("isogloss")) ;
+        {
             points += 3;
         }
     }
@@ -35,10 +43,15 @@ public class MainActivity extends AppCompatActivity {
         CheckBox egg = findViewById(R.id.egg);
         CheckBox banana = findViewById(R.id.banana);
         CheckBox leaflet = findViewById(R.id.leaflet);
+        CheckBox throwBox = findViewById(R.id.Throw);
+        CheckBox fast = findViewById(R.id.fast);
+        CheckBox slowly = findViewById(R.id.slowly);
 
-        if (egg.isChecked() && banana.isChecked() && leaflet.isChecked()) {
+        if (egg.isChecked() && banana.isChecked() && leaflet.isChecked() && !throwBox.isChecked()
+                && !fast.isChecked() && !slowly.isChecked()) {
             points += 3;
         }
+
     }
 
     //This function checks if correct answer is given
@@ -60,19 +73,20 @@ public class MainActivity extends AppCompatActivity {
         openQuestion();
 
         //This creates the Toast and shows it
-
         String cheer = "";
         if (points <= 1) {
             cheer = "You can do a lot better!";
         } else if (points <= 3) {
             cheer = "Come on, try harder!";
-        } else if (points <= 6) {
+        } else if (points <= 7) {
             cheer = "Not to bad!";
+        } else if (points <= 9) {
+            cheer = "Almost perfect!";
         } else {
-            cheer = "Perfect";
+            cheer = "Perfect!";
         }
 
-        String points_total = points + "/7 points. " + cheer;
+        String points_total = points + "/10 points. " + cheer;
         Context context = getApplicationContext();
         CharSequence text = points_total;
         int duration = Toast.LENGTH_LONG;
